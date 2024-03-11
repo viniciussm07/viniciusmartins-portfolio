4bd446704a3568b6c18bbb797ebf4c66aa075f98
viniciussm07 <vini.soares.martins@gmail.com>
2024-03-11 15:58:19 -0300

    Flat: Adding Tripppin Project in Portfolio section

--- data/en/portfolio.ts
+++ data/en/portfolio.ts
@@ -1,22 +1,22 @@
-import { ProjectsInterface } from "../../components/section/portfolio/types"
+import { ProjectsInterface } from '../../components/section/portfolio/types'
 
 const projects: ProjectsInterface[] = [
   {
     name: 'Sustento Energia Solar Website + Blog',
-    description: 'Website and blog made focused in SEO and have a good performance in fotovoltaic integrating field.',
+    description:
+      'Website and blog made focused in SEO and have a good performance in fotovoltaic integrating field.',
     imgSource: '/images/projects/website-sustento-min.webp',
     skills: ['React.js', 'Next.js', 'Styled-components', 'Figma'],
-    websiteUrl: 'https://sustentoenergiasolar.com.br/',
+    websiteUrl: 'https://sustentoenergiasolar.com.br/'
   },
   {
-    name: 'Project Name',
-    description: 'Project Description',
-    imgSource: '/images/projects/website-sustento-min.webp',
-    skills: ['Next.js', 'GraphQL', 'Apollo'],
-    websiteUrl: 'https://github.com/viniciussm07',
-    githubUrl: 'https://github.com/viniciussm07',
-  },
+    name: 'Tripppin Platform',
+    description:
+      'Configuration and deployment of an EC2 instance on AWS, along with the creation of a Route 53 configuration to associate the domain with the instance. Additionally, I have developed a bash script to automate the deployment process.',
+    imgSource: '/images/projects/tripppin-platform-min.webp',
+    skills: ['AWS', 'EC2', 'Route53', 'Bash'],
+    websiteUrl: 'https://tripppin.com/'
+  }
 ]
-
 
 export default projects
